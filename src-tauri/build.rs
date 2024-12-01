5eb5f8d77b47b296ce7230da39fde004b8b99f94
mstomar698 <tomarm698@gmail.com>
2024-12-01 06:47:59 +0530

    changes 5

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,18 @@
+use std::path::Path;
+
 fn main() {
-    tauri_build::build()
+    let kuzu_lib_path = Path::new("kuzu/libkuzu.dylib");
+
+    if kuzu_lib_path.exists() {
+        println!("cargo:rerun-if-changed={}", kuzu_lib_path.display());
+
+        println!("cargo:rustc-link-lib=dylib=kuzu");
+
+        println!(
+            "cargo:rustc-link-search=native={}",
+            kuzu_lib_path.parent().unwrap().display()
+        );
+    } else {
+        panic!("KuzuDB library not found at {:?}", kuzu_lib_path);
+    }
 }
