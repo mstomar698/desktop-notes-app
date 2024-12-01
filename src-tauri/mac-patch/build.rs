d2f16713214fbfc7b0ea136c90a33d76d7f4c32b
mstomar698 <tomarm698@gmail.com>
2024-12-01 06:04:13 +0530

    changes

---
+++ src-tauri/mac-patch/build.rs
@@ -0,0 +1,17 @@
+use std::env;
+          use std::path::Path;
+
+          fn main() {
+              let kuzu_lib_path = Path::new("src-tauri/kuzu/libkuzu.dylib");
+
+              if kuzu_lib_path.exists() {
+                  println!("cargo:rerun-if-changed={}", kuzu_lib_path.display());
+                  println!("cargo:rustc-link-lib=dylib=kuzu");
+                  println!(
+                      "cargo:rustc-link-search=native={}",
+                      kuzu_lib_path.parent().unwrap().display()
+                  );
+              } else {
+                  panic!("KuzuDB library not found at {:?}", kuzu_lib_path);
+              }
+          }
